6218090d34c63ad912940cd60ac36ffc00edcd73
Arthur Levitsky <artsky.task@gmail.com>
2026-04-21 01:08:28 -0400

    created discretized agent

--- homework3.py
+++ homework3.py
@@ -41,12 +41,18 @@ def modifyReward(next_state):
     # We scale reward from velocity higher to make sure that the agent doesn't just prioritize being on the slopes of the hills. 
     return abs(velocity) * 20 + abs(position + 0.5)
 
-def discretizeState(state):
+def discretizeState(state, env, pos_bins=20, vel_bins=20):
     """
     Discretize the state. Used with value iteration.
     """
-    # This is part of your homework
-    pass
+    pos_low, vel_low = env.observation_space.low
+    pos_high, vel_high = env.observation_space.high
+    position, velocity = state
+
+    pos_idx = int(np.digitize(position, np.linspace(pos_low, pos_high, pos_bins)))
+    vel_idx = int(np.digitize(velocity, np.linspace(vel_low, vel_high, vel_bins)))
+
+    return (pos_idx, vel_idx)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -75,9 +81,9 @@ if __name__ == "__main__":
     max_steps = 4000
     env = gym.make('MountainCarContinuous-v0', max_episode_steps=max_steps)
 
-    # We can go up, down, left, or right
+    # Continuous actions representing directional force applied to car.
     print(f"Action space: {env.action_space}")
-    # We know the vehicle position and speed
+    # We know the vehicle position and speed.
     print(f"Observation space: {env.observation_space}")
 
     # By default, program will run the DQN approach.
@@ -90,7 +96,7 @@ if __name__ == "__main__":
         target_dqn = DQN(state_dim=2, action_dim=num_actions)
 
         target_dqn.load_state_dict(policy_dqn.state_dict())
-        target_dqn.eval()
+        target_dqn.eval() # Target network will not be training.
 
         optimizer = torch.optim.Adam(policy_dqn.parameters(), lr=0.001)
         loss_fn = nn.MSELoss()
@@ -110,7 +116,6 @@ if __name__ == "__main__":
 
             # Run the simulation
             finished = False
-            is_training = False
 
             sim_steps = 0
             episode_reward = 0.0
@@ -136,10 +141,8 @@ if __name__ == "__main__":
                 # Storing the experience in the replay buffer.
                 replay_buffer.append((state, action_idx, reward, next_state, terminated))
 
-                is_training = len(replay_buffer) >= batch_size
-
                 # If we have enough experiences in replay buffer, begin training.
-                if is_training:
+                if len(replay_buffer) >= batch_size:
                     batch = random.sample(replay_buffer, batch_size)
                     states, actions_batch, rewards, next_states, dones = zip(*batch)
 
@@ -185,9 +188,76 @@ if __name__ == "__main__":
 
             print(f"Episode {episode + 1}/{args.episodes} | Steps: {sim_steps} | Total Reward: {episode_reward:.2f} | Epsilon: {epsilon:.3f}")
     
+    # run discrete agent by using argument --algorithm discretized
+    elif args.algorithm == 'discretized': 
+        pos_bins = 10
+        vel_bins = 10
+        discrete_actions = [-1.0, -0.5, 0.0, 0.5, 1.0]
+        num_actions = len(discrete_actions)
+
+        q_table = np.zeros((pos_bins + 1, vel_bins + 1, num_actions))
+        flag_state = discretizeState([0.45, 0.0], env, pos_bins, vel_bins)
+        flag_pos_idx, flag_vel_idx = flag_state
+        q_table[flag_pos_idx, :, :] = 100.0
+
+        # Seed high velocity states on the left hill as valuable
+        left_hill_state = discretizeState([-1.0, 0.0], env, pos_bins, vel_bins)
+        left_hill_pos_idx = left_hill_state[0]
+
+        # High rightward velocity columns (right half of vel axis)
+        q_table[left_hill_pos_idx, vel_bins//2:, :] = 25.0
+
+        max_q = np.max(q_table, axis=2)
+        np.savetxt("q_table.csv", max_q, delimiter=',', fmt='%.2f')
+
+        alpha = 0.5 
+        gamma = 0.99
+        epsilon = 1.0 # Start with 100% random actions
+        epsilon_min = 0.01 # Never explore less than 1% of the time
+        target_episode = args.episodes * 10 / 2
+        epsilon_decay = (epsilon_min / epsilon) ** (1 / target_episode) # Multiply epsilon by this after each episode
+
+        
+        for episode in range(args.episodes * 10):
+            # Reset the environment to put the agent into an initial state
+            state, info = env.reset()
+
+            # Run the simulation
+            finished = False
+            state = discretizeState(state, env, pos_bins, vel_bins)
+            sim_steps = 0
+            sim_steps_rate = 500
+
+            while not(finished):
+                sim_steps += 1
+
+                if random.random() < epsilon: 
+                    action_idx = random.randint(0, num_actions -1)
+                else: 
+                    action_idx = np.argmax(q_table[state])
+
+                action = [discrete_actions[action_idx]]
+                next_state, env_reward, terminated, truncated, info = env.step(action)
+                next_state = discretizeState(next_state, env, pos_bins, vel_bins)
+
+                # update Q-table
+                best_next_q = 0 if terminated else np.max(q_table[next_state])
+                q_table[state][action_idx] += alpha * (env_reward + gamma * best_next_q - q_table[state][action_idx])
+                    
+                state = next_state
+                finished = terminated or truncated
+
+            # Decay epsilon per episode to decrease exploration.
+            epsilon = max(epsilon_min, epsilon * epsilon_decay)
+
+            print(f"Episode {episode+1}/{args.episodes * 10} | Steps: {sim_steps} | Epsilon: {epsilon:.3f}")
+
+        max_q = np.max(q_table, axis=2)
+        np.savetxt("q_table_end.csv", max_q, delimiter=',', fmt='%.2f')
+
     env.close()
 
-    # Play with policy
+    # Play with policy. If using --record argument, video will be created in directory of file.
     if args.record:
         env = gym.make('MountainCarContinuous-v0', max_episode_steps=4000, render_mode="rgb_array")
         env = gym.wrappers.RecordVideo(
@@ -198,19 +268,22 @@ if __name__ == "__main__":
         )
     else:
         env = gym.make('MountainCarContinuous-v0', max_episode_steps=4000, render_mode="human")
+    
     state, info = env.reset()
     # Run the simulation
     finished = False
 
     while not(finished):
         if args.algorithm == 'discretized':
+            pos_bins = 10
+            vel_bins = 10
+
+
             # The discretized model should not require learning, converging instead through value iteration.
-            state = discretizeState(state)
-            next_state = discretizeState(next_state)
-            # TODO Get the next action from the discretized model
-            action = [0]
+            state_d = discretizeState(state, env, pos_bins, vel_bins)
+            action = [discrete_actions[np.argmax(q_table[state_d])]]
         else:
-            # TODO Get the next action from your DQN
+            # Get the next action from your DQN
             state_tensor = torch.FloatTensor(state).unsqueeze(0)
             with torch.no_grad():
                 q_values = policy_dqn(state_tensor)
